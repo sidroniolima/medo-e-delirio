99e20e4d988045ecef2febcfdd3e2b24a5bada98
Sidronio <sidronio@sidroniolima.com.br>
2021-03-04 16:29:29 -0300

    Sliver no bottom com @criado

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -35,72 +35,64 @@ class _HomeState extends State<Home> {
 
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              height: screenSize.height * .945,
-              child: CustomScrollView(slivers: [
-                SliverAppBar(
-                    pinned: false,
-                    expandedHeight: screenSize.height * .25,
-                    toolbarHeight: 0,
-                    flexibleSpace: FlexibleSpaceBar(
-                      background: Image.asset(
-                        'assets/images/med_full_logo.png',
-                        fit: BoxFit.fitHeight,
-                      ),
-                    )),
-                SliverGrid.count(
-                    crossAxisCount: 3,
-                    children: Audio.inserts
-                        .map((Audio audio) => MediaPanel(
-                              author: audio.author,
-                              isPlaying: actualIdPlayind == audio.id,
-                              onLongPress: () async {
-                                setState(() {
-                                  this.actualIdPlayind = audio.id;
-                                });
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+                pinned: false,
+                expandedHeight: screenSize.height * .25,
+                toolbarHeight: 0,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image.asset(
+                    'assets/images/med_full_logo.png',
+                    fit: BoxFit.fitHeight,
+                  ),
+                )),
+            SliverGrid.count(
+                crossAxisCount: 3,
+                children: Audio.inserts
+                    .map((Audio audio) => MediaPanel(
+                          author: audio.author,
+                          isPlaying: actualIdPlayind == audio.id,
+                          onLongPress: () async {
+                            setState(() {
+                              this.actualIdPlayind = audio.id;
+                            });
 
-                                Uint8List bytes = await readBytes(
-                                    'https://sidroniolima.com.br/med/mp3/${audio.fileName}');
-                                await Share.file('Sound', audio.fileName,
-                                    bytes.buffer.asUint8List(), 'audio/*');
+                            Uint8List bytes = await readBytes(
+                                'https://sidroniolima.com.br/med/mp3/${audio.fileName}');
+                            await Share.file('Sound', audio.fileName,
+                                bytes.buffer.asUint8List(), 'audio/*');
 
-                                setState(() {
-                                  this.actualIdPlayind = -1;
-                                });
-                              },
-                              onPress: () async {
-                                setState(() {
-                                  this.actualIdPlayind = audio.id;
-                                });
+                            setState(() {
+                              this.actualIdPlayind = -1;
+                            });
+                          },
+                          onPress: () async {
+                            setState(() {
+                              this.actualIdPlayind = audio.id;
+                            });
 
-                                Uint8List bytes = await readBytes(
-                                    'https://sidroniolima.com.br/med/mp3/${audio.fileName}');
+                            Uint8List bytes = await readBytes(
+                                'https://sidroniolima.com.br/med/mp3/${audio.fileName}');
 
-                                audioPlayer.setVolume(1.0);
-                                await audioPlayer.playBytes(bytes);
+                            audioPlayer.setVolume(1.0);
+                            await audioPlayer.playBytes(bytes);
 
-                                setState(() {
-                                  this.actualIdPlayind = -1;
-                                });
-                              },
-                              screenSize: screenSize,
-                              label: audio.label,
-                            ))
-                        .toList()),
-              ]),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(8.0),
-                      bottomRight: Radius.circular(8.0))),
-              child: Text(
+                            setState(() {
+                              this.actualIdPlayind = -1;
+                            });
+                          },
+                          screenSize: screenSize,
+                          label: audio.label,
+                        ))
+                    .toList()),
+            SliverAppBar(
+              pinned: false,
+              expandedHeight: screenSize.height * .05,
+              centerTitle: true,
+              title: Text(
                 'criado por @sidroniolima',
-                style: TextStyle(
-                  fontSize: screenSize.width * .03,
-                ),
+                style: TextStyle(fontSize: screenSize.width * .032),
               ),
             ),
           ],
